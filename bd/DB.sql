a06cd1a2d2ae7a63d833ddf0863d6330d9ca790b
Joel Tancara S. <140029048+JoeTancara@users.noreply.github.com>
2024-12-15 20:57:09 -0700

    Delete bd directory

--- bd/DB.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE `platos` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `procedencia` varchar(45) NOT NULL,
-  `receta` varchar(45) NOT NULL,
-  `foto` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
